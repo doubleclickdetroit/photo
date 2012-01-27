34334b05715d359d33a6d6619cb67378cc4d11d9
Brad Chase <bchase@doubleclickdetroit.com>
2012-01-27 17:08:17 -0500

    added some testing stuff

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
+# require 'factory_girl'
 
-FactoryGirl.define do
-  factory :user do
-  end
+Factory.define :user do |f|
+  f.sequence(:name) {|n| "foo{n}"}
+  f.sequence(:email) {|n| "foo#{n}@bar.com"}
+  f.password "foobarbaz"
+  f.password_confirmation { |u| u.password }
 end
---
+++ spec/requests/devise_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'uri'
+
+describe 'Devise HTTP Auth' do
+  before(:each) { @user = Factory(:user) }
+
+  describe "sign in" do
+    it "returns success" do
+      pending
+
+      # uri = URI(user_session_url)
+
+      # req = Net::HTTP::Get.new(uri.request_uri)
+      # req.basic_auth @user.email, @user.password 
+
+      # res = Net::HTTP.start(uri.host, uri.port) {|http|
+      #   http.request(req)
+      # }
+      # puts res.body
+
+      # str  = "#{@user.email}:#{@user.password}"
+      # pair = URI.unescape(CGI::escape(Base64.decode64(str)))
+      # # header 'Authorization', "Basic  #{pair}"
+      # post user_session_url, {"Authorization" => "Basic  #{@user.email + ":" + @user.password}"} 
+      response.should contain('Signed in successfully')
+    end
+  end
+
+  # describe "sign in" do
+  #   it "returns http success" do
+  #     get 'index'
+  #     response.should be_success
+  #   end
+  # end
+end
