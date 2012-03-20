e6c568c70a22651074b39c7e783e0cecad7c994c
Brad Chase <bchase@doubleclickdetroit.com>
2012-03-19 23:14:08 -0400

    refactored asset and added tests

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -7,12 +7,7 @@ class Asset < ActiveRecord::Base
   # allow the following to be used without the redundant
   # prefix 'file_' they come with from paperclip
   %w(file_name file_size content_type url).each do |m_name|
-    # todo figure out why this is magic
-    # do not remove
-    self.new
-    # do not remove
-
     new, old = m_name.intern, ("file_"+m_name).intern
-    alias_method new, old 
+    define_method new, lambda { self.send old }
   end
 end
--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -1,4 +1,40 @@
 require 'spec_helper'
 
 describe Asset do
+  before(:each) { @asset = Factory(:asset) }
+
+  describe '#url' do
+    it 'should be delegated to file' do
+      @asset.file.should_receive :url
+      @asset.url
+    end
+  end
+
+  describe '#file_name' do
+    it 'should call #file_file_name' do
+      @asset.should_receive :file_file_name
+      @asset.file_name
+    end
+  end
+
+  describe '#file_size' do
+    it 'should call #file_file_size' do
+      @asset.should_receive :file_file_size
+      @asset.file_size
+    end
+  end
+
+  describe '#content_type' do
+    it 'should call #file_content_type' do
+      @asset.should_receive :file_content_type
+      @asset.content_type
+    end
+  end
+
+  describe '#url' do
+    it 'should call #file_url' do
+      @asset.should_receive :file_url
+      @asset.url
+    end
+  end
 end
