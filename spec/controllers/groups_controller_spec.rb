f11df6e3dca74256931e2657f2846aa5aa4c215e
Brad Chase <bchase@doubleclickdetroit.com>
2012-03-20 16:25:09 -0400

    added on to group controller spec

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -68,6 +68,189 @@ describe GroupsController do
 
   end
 
+
+  context 'admin for group logged in' do
+    login_admin()
+
+    describe "GET index" do
+      # it "assigns all groups as @groups" do
+      #   group = Factory(:group)
+      #   get :index, {}, valid_session
+      #   assigns(:groups).should eq([group])
+      # end
+    end
+
+    describe "GET show" do
+      it "assigns the requested group as @group" do
+        group = Factory(:group)
+        get :show, {:id => group.to_param}, valid_session
+        assigns(:group).should eq(group)
+      end
+    end
+
+    # describe "GET new" do
+    #   it "assigns a new group as @group" do
+    #     get :new, {:group_id => @group.id}, valid_session
+    #     assigns(:group).should be_a_new(Group)
+    #   end
+    # end
+
+    # describe "GET edit" do
+    #   it "assigns the requested group as @group" do
+    #     group = Factory(:group)
+    #     get :edit, {:id => group.to_param}, valid_session
+    #     assigns(:group).should eq(group)
+    #   end
+    # end
+
+    describe "POST create" do
+      let(:valid_attributes) { Factory.build(:group).attributes }
+
+      describe "with valid params" do
+        it "creates a new Group" do
+          expect {
+            post :create, {:group => valid_attributes}, valid_session
+          }.to change(Group, :count).by(1)
+        end
+
+        # it "assigns a newly created group as @group" do
+        #   post :create, {:group => valid_attributes}, valid_session
+        #   assigns(:group).should be_a(Group)
+        #   assigns(:group).should be_persisted
+        # end
+
+        # it "redirects to the created group" do
+        #   post :create, {:group => valid_attributes}, valid_session
+        #   response.should redirect_to(Group.last)
+        # end
+      end
+
+      describe "with invalid params" do
+        it "assigns a newly created but unsaved group as @group" do
+          # Trigger the behavior that occurs when invalid params are submitted
+          Group.any_instance.stub(:save).and_return(false)
+          post :create, {:group => {}}, valid_session
+          assigns(:group).should be_a_new(Group)
+        end
+
+        # it "re-renders the 'new' template" do
+        #   # Trigger the behavior that occurs when invalid params are submitted
+        #   Group.any_instance.stub(:save).and_return(false)
+        #   post :create, {:group => {}}, valid_session
+        #   response.should render_template("new")
+        # end
+      end
+
+    end
+
+    describe "PUT update" do
+      let(:valid_attributes) do
+        { :name => 'Group PUT' }
+      end
+
+      describe "with valid params" do
+        # it "updates the requested group" do
+        #   group = Factory(:group)
+        #   # Assuming there are no other groups in the database, this
+        #   # specifies that the Group created on the previous line
+        #   # receives the :update_attributes message with whatever params are
+        #   # submitted in the request.
+        #   Group.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
+        #   put :update, {:id => group.to_param, :group => {'these' => 'params'}}, valid_session
+        # end
+
+        it "assigns the requested group as @group" do
+          group = Factory(:group) 
+          put :update, {:id => group.to_param, :group => valid_attributes}, valid_session
+          assigns(:group).should eq(group)
+        end
+
+        # it "redirects to the group" do
+        #   group = Factory(:group) 
+        #   put :update, {:id => group.to_param, :group => valid_attributes}, valid_session
+        #   response.should redirect_to(group)
+        # end
+      end
+
+      describe "with invalid params" do
+        it "assigns the group as @group" do
+          group = Factory(:group) 
+          # Trigger the behavior that occurs when invalid params are submitted
+          Group.any_instance.stub(:save).and_return(false)
+          put :update, {:id => group.to_param, :group => {}}, valid_session
+          assigns(:group).should eq(group)
+        end
+
+        # it "re-renders the 'edit' template" do
+        #   pending 'may not need edit'
+        #   group = Factory(:group) 
+        #   # Trigger the behavior that occurs when invalid params are submitted
+        #   Group.any_instance.stub(:save).and_return(false)
+        #   put :update, {:id => group.to_param, :group => {}}, valid_session
+        #   response.should render_template("edit")
+        # end
+      end
+    end
+
+    describe "DELETE destroy" do
+      # it "destroys the requested group" do
+      #   group = Factory(:group)  
+      #   expect {
+      #     delete :destroy, {:id => group.to_param}, valid_session
+      #   }.to change(Group, :count).by(-1)
+      # end
+
+      it "redirects to the groups list" do
+        pending 'no idea why this throws a 406'
+        group = Factory(:group, :group_id => @group.id)  
+        delete :destroy, {:id => group.to_param}, valid_session
+        response.should redirect_to(groups_url)
+      end
+    end
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #   describe "GET index" do
 #     it "assigns all groups as @groups" do
 #       pending 'broke this with making group_controller#create add users to new group'
