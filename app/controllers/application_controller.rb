60814caee8d3fb6fd7b65d2be00ae041016464ef
Brad Chase <bchase@doubleclickdetroit.com>
2012-03-19 22:06:06 -0400

    http basic auth for prod (heroku) user/pass spongebob/squarepants :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :http_authenticate if Rails.env.production?
+
+protected
+
+  def http_authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == "spongebob" && password == "squarepants"
+    end
+    warden.custom_failure! if performed?
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
